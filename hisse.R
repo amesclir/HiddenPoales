0295119451294b50ed5026fe392df1fe0c04e6a5
amesclir <amesclir@gmail.com>
2022-10-10 16:37:04 +0200

    open closed done

---
+++ hisse.R
@@ -0,0 +1,18 @@
+###
+# closed = 0
+# open = 1
+# annual = 0
+# perennial = 1
+# wind = 0
+# animal = 1
+# silica absent = 0
+# silica present = 1
+# dry = 0
+# wet = 1
+# epiphyte no = 0
+# epiphyte yes = 1
+# open + dry = 0
+# open + wet = 1
+# C3 = 0
+# C4 = 1
+# CAM = 1
